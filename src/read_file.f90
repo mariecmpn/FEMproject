e36cfe63939c70b4273171d7f6c7cb9415317933
Marie COMPAIN <mariecompain@MacBook-Pro-de-Marie-2.local>
2023-12-18 11:15:25 +0100

    added type resolution if loop

--- src/read_file.f90
+++ src/read_file.f90
@@ -4,13 +4,14 @@ module read_file
 
     contains
 
-    subroutine open_file(file_name, meshfile_name, save_file, stock, degre, cond)
+    subroutine open_file(file_name, meshfile_name, save_file, stock, degre, cond, res)
         character(len=*), intent(in) :: file_name
         character(len=70), intent(inout) :: meshfile_name
         character(len=70), intent(inout) :: save_file
         integer, intent(inout) :: stock
         integer, intent(inout) :: degre
         real(rp), intent(inout) :: cond
+        character(len = 1), intent(inout) :: res
         integer :: my_unit = 60
 
         open(my_unit, file = file_name, action = 'read', form = 'formatted', status = 'old')
@@ -20,6 +21,9 @@ module read_file
         read(my_unit,*) stock
         read(my_unit,*) degre
         read(my_unit,*) cond
+        read(my_unit,*) res
+
+        close(my_unit)
     end subroutine open_file
 
 end module read_file
